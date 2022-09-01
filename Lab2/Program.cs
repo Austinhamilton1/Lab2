3a660cbe208585adf8e5976dccc2b99d73413ac4
Austin Hamilton <HAMILTONAJ@ETSU.EDU>
2022-09-01 16:43:36 -0400

    base code

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Welcome();
+
+static void Welcome()
+{
+    Console.WriteLine("Welcome to Lab 2!");
+    Console.WriteLine("This is a lab that show cases some basic C# functionality.");
+    Console.WriteLine("Press any key to get started...");
+    Console.ReadKey();
+    Console.Clear();
+}
